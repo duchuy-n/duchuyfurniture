b877534a04761d037eb70bedec612ad3a8f74916
duxyr. <90257558+duchuy-n@users.noreply.github.com>
2026-08-30 22:02:39 +0700

    Update product: Hòa phát ATH5115

--- products.js
+++ products.js
@@ -3,8 +3,8 @@ window.recoveredProducts = [
     "id": "307144",
     "title": "Hòa phát ATH5115",
     "code": "ATH5115",
-    "price": 7630005,
-    "priceText": "7.630.005 VNĐ",
+    "price": 7630000,
+    "priceText": "7.630.000 VNĐ",
     "vatPrice": "8.393.000 VNĐ (Đã bao gồm VAT)",
     "size": "W5100x D1500 x H750 mm",
     "warranty": "Sản phẩm bảo hành 12 tháng",
@@ -19,7 +19,7 @@ window.recoveredProducts = [
     "slug": "hoa-phat-ath5115-ath5115",
     "detailPath": "san-pham/hoa-phat-ath5115-ath5115-307144/",
     "createdAt": "2026-08-30T14:56:47.124Z",
-    "updatedAt": "2026-08-30T14:58:25.717Z"
+    "updatedAt": "2026-08-30T15:02:39.676Z"
   },
   {
     "id": "307145",
